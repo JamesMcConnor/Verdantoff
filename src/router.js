31332b700de1655b1dca0678766aa2c9d09c97a4
EririMaki <88325284+EririMaki@users.noreply.github.com>
2022-05-04 16:04:12 +1200

    fix routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,12 @@
 import {createRouter,createWebHashHistory} from "vue-router";
 import About from './components/About';
-import Calendar from './components/Calendar';
-import Markdown from './components/Markdown';
-import Chat from './components/Chat';
+
+
 import LogChat from './components/LogChat';
 
 
 const routes = [
     {path:'/about',component: About},
-    {path:'/calendar',component: Calendar},
-    {path:'/markdown',component: Markdown},
-    {path:'/chat',component: Chat},
     {path:'/logchat',component: LogChat},
     {path:'/',component: About}
 ]
