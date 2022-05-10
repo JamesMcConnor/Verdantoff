4a833a65abc47ce70026cf28635e89b389eb2e5b
SolaireTi <79572077+SolarTi@users.noreply.github.com>
2022-05-10 17:19:07 +1200

    Fixed Display Issue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,35 +1,53 @@
 <template>
-<div :style = "background" class = "relative bg-fixed opacity-90 w-full h-screen">
-
-<h1 class="pt-40 underline text-center mx-auto  text-4xl text-green-500 italic font-serif">Welcome to Verdantoff</h1>
-<p class="pt-5 underline text-center mx-auto  text-4xl text-green-500 italic font-serif">Your virtual office online</p>
-<p class="pt-10 text-center mx-auto  text-2xl text-green-500 font-serif">Please Sign Up or Sign In to use the services on this site</p>
-</div>
-
-<!---<Footer></Footer>--->
+  <div :style="background" class="relative bg-fixed opacity-90 w-full h-screen">
+    <h1
+      class="
+        pt-40
+        underline
+        text-center
+        mx-auto
+        text-4xl text-green-500
+        italic
+        font-serif
+      "
+    >
+      Welcome to Verdantoff
+    </h1>
+    <p
+      class="
+        pt-5
+        underline
+        text-center
+        mx-auto
+        text-4xl text-green-500
+        italic
+        font-serif
+      "
+    >
+      Your virtual office online
+    </p>
+    <p class="pt-10 text-center mx-auto text-2xl text-green-500 font-serif">
+      Please Sign Up or Sign In to use the services on this site
+    </p>
+  </div>
 </template>
 
 <script>
-//import Footer from './Footer.vue';
-
 export default {
-    data(){
-      return {
-        background: {
-          backgroundImage: 'url(' + require('../ocean.jpg') + ')',
-          backgroundRepeat:'no-repeat',
-          backgroundSize: 'cover',
-          backgoundPosition: 'center'
-        }
-      }
-    },
+  data() {
+    return {
+      background: {
+        backgroundImage: "url(" + require("../ocean.jpg") + ")",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgoundPosition: "center",
+      },
+    };
+  },
 
-    components: {
-      //Footer
-    }
+  components: {},
 };
 </script>
 
 <style>
-
 </style>
