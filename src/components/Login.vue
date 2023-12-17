8f2649f7a6bd008ecf3ec6242e5e1f49b86e5b65
Faizan Ahmed Raza <faizanahmedraza3@gmail.com>
2023-12-17 17:54:43 +0500

    feat: responsive design issue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,49 +1,41 @@
-<!---
-This is the login page. 
- --->
-
-
 <template>
-<div>
-<section 
-  @click="close"
-  class="z-20 h-screen w-screen bg-white gold-text fixed top-0 opacity-50"></section>
-  <!-- Use Z-Axis to make the button higher than others -->
-  <div class="absolute inset-0">
-    <div class="flex h-full">
+  <div>
+    <section
+      @click="close"
+      class="z-20 h-screen w-screen bg-white gold-text fixed top-0 opacity-50"
+    ></section>
+    <!-- Use Z-Axis to make the button higher than others -->
+    <div class="absolute inset-0 flex items-center justify-center">
       <!---Login window--->
-      <div class="z-30 m-auto bg-black gold-text p-2 rounded shadow w-1/3">
+      <div class="z-30 bg-black gold-text p-4 rounded shadow-md md:w-1/2 lg:w-1/3">
         <h1 class="text-xl text-center">Login</h1>
         <form class="p-2 my-4 border" @submit.prevent="submit">
           <div class="my-2">
-           <label>Email</label>
-          <input v-model="form.email" class="rounded shadow p-2 w-full" type="email"> 
+            <label>Email</label>
+            <input v-model="form.email" class="rounded shadow p-2 w-full" type="email" />
           </div>
           <div class="my-2">
-           <label>Password</label>
-          <input v-model="form.password" class="rounded shadow p-2 w-full" type="password"> 
+            <label>Password</label>
+            <input v-model="form.password" class="rounded shadow p-2 w-full" type="password" />
           </div>
           <div class="my-2">
-           <button type="submit" class="p-2 w-full rounded shadow bg-red-400 text-white">
-             <span v-if="!isLoading">Submit</span>
-             <span v-else>Processing</span>
-          </button>
-          <!---Shows if user enter a wrong password or email--->
-          <span v-if="isError" class="my-2 text-red-400">The email or password you entered is wrong</span>
+            <button type="submit" class="p-2 w-full rounded shadow bg-gold text-black hover:text-white">
+              <span v-if="!isLoading">Submit</span>
+              <span v-else>Processing</span>
+            </button>
+            <!---Shows if the user enters a wrong password or email--->
+            <span v-if="isError" class="my-2 text-red-400">The email or password you entered is wrong</span>
           </div>
         </form>
         <div class="my-2">
           <!--- Button for reset password --->
-            <button @click="reset" class = "p-2 w-1/3 rounded shadow bg-green-400 text-white">Forget Password?</button>
-            <span v-if="isSend" class="my-2 p-2 text-red-400" >Please check your email for reset password</span>
+          <button @click="reset" class="p-2 w-full rounded shadow bg-red-400 text-white">Forget Password?</button>
+          <span v-if="isSend" class="my-2 p-2 text-red-400">Please check your email for reset password</span>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
-
-
 
 <script>
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
@@ -62,7 +54,7 @@ export default {
       isError: false,
       isLoading: false,
       isSend: false,
-    }
+    };
   },
 
   methods: {
@@ -76,8 +68,7 @@ export default {
         toast.error("Email and password are required fields.");
         this.isLoading = false;
       } else {
-
-        //Get status of authentication
+        //Get the status of authentication
         const auth = getAuth(app);
         signInWithEmailAndPassword(auth, this.form.email, this.form.password)
           .then(() => {
@@ -96,7 +87,7 @@ export default {
             this.isLoading = false;
             // this.isError = true;
             this.$forceUpdate();
-            toast.error(errorMessage)
+            toast.error(errorMessage);
           });
       }
     },
@@ -113,7 +104,7 @@ export default {
         this.$forceUpdate();
         sendPasswordResetEmail(auth, this.form.email)
           .then(() => {
-            toast.success("Password reset email sent!")
+            toast.success("Password reset email sent!");
           })
           .catch((error) => {
             const errorMessage = error.message || "Something went wrong";
@@ -122,9 +113,9 @@ export default {
       }
     },
   },
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+/* Add your scoped styles here */
 </style>
