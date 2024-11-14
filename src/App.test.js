fda6620cddc689c845f210c7ebd7d3d9c778d978
james <jadconnor@gmail.com>
2024-11-14 20:05:13 +1300

    Update App.test.js to reflect current app content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Live Video Streaming with Chat header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Live Video Streaming with Chat/i);
+  expect(headerElement).toBeInTheDocument();
 });
